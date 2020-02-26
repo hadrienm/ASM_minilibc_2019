22837d203dda833335da3f9f45f7f57b5eb1120d
Adrien Héros <48791142+hadrienm@users.noreply.github.com>
2020-02-26 19:28:47 +0100

    update memcpy

--- memcpy.s
+++ memcpy.s
@@ -5,6 +5,8 @@ memcpy:
     push rbp                ; Prologue
     mov rbp, rsp            ; (==enter)
 
+    mov rax, rdi            ; result = dst
+    mov r9b, 0              ; char temp = NULL
     mov r10, 0              ; i = 0
     cmp rsi, 0              ; if (str == NULL)
     je .end                 ; return NULL
@@ -12,10 +14,11 @@ memcpy:
     je .end                 ; return NULL
 
 .loop:                      ; while
-    cmp rdx, r10            ; if (i == index)
-    je .end
-    mov rdi, [rsi + r10]    ; newstr[i] = str[i];
-    inc r10                 ; ++i
+    cmp edx, r10d           ; if (i == index)
+    je .end                 ; return dst
+    mov r9b, [rsi + r10]    ; temp = src[i]
+    mov [rdi + r10], r9b    ; dst[i] = tmp;
+    inc r10d                ; ++i
     jmp .loop
 
 .end:
