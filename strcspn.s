7e48441d7fba057133df208cc17709c2056330ae
Adrien Héros <48791142+hadrienm@users.noreply.github.com>
2020-02-28 11:55:18 +0100

    correct bug count of strcspn function

--- strcspn.s
+++ strcspn.s
@@ -1,27 +1,33 @@
 BITS 64
-global stcspn
+global strcspn
 
 strcspn:
     push rbp                        ; Prologue
     mov rbp, rsp                    ; (==enter)
     mov rax, 0                      ; result = NULL
     mov r8d, 0                      ; int tmp_index = 0
-    mov r9d, 0                      ; int i = 0
+    mov r9d, 0                      ; int i = 1
     mov r10b, 0                     ; char tmp = NULL
 
 .loop:
     mov r8d, 0                      ; tmp_index = 0
-    cmp byte [rdi], 0                    ; s[i] == NULL
+    cmp byte [rdi], 0               ; s[i] == NULL
     je .setreturn
-    inc rdi                         ; ++s
     jmp .secondloop                 ; double for
     
 .secondloop:
-    mov r10b, [rsi + r8]
-    cmp [rdi], r10b                 ; if (s[i] == charset[i])
+    mov r10b, [rsi + r8]            ; tmp = charset[i]
+    cmp r10b, 0                     ; if (tmp == NULL)
+    je .inc
+    cmp [rdi], r10b                 ; if (s[i] == tmp)
     je .setreturn
-    inc r9d                         ; ++i
     inc r8d                         ; ++tmp_index
+    jmp .secondloop
+
+.inc:
+    inc rdi                         ; ++s
+    inc r9d                         ; ++i
+    jmp .loop
 
 .setreturn:
     mov rax, r9                     ; result = i
